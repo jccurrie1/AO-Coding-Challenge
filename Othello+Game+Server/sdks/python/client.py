d89b4e8052360984fd1ea668d16a776b6bfee78c
Jared Currie <38235677+jccurrie1@users.noreply.github.com>
2023-12-15 22:01:07 -0500

    adding both diagonal and horizontal checks. Similar to my vertical check

--- Othello+Game+Server/sdks/python/client.py
+++ Othello+Game+Server/sdks/python/client.py
@@ -13,19 +13,38 @@ def get_move(player, board):
   # TODO determine valid moves
 
   # need to check horizontal
+  horizontal_check(player, board, 'top_to_bottom')
+  horizontal_check(player, board, 'bottom_to_top')
 
   # need to check vertical
   vertical_check(player, board, 'l_to_r')
   vertical_check(player, board, 'r_to_l')
 
-  print(valid_moves)
-
-
-
   # need to check diagonal
+  diagonal_check(player, board, 'top_left_to_bottom_right')
+  diagonal_check(player, board, 'bottom_right_to_top_left')
+
+  print(valid_moves)
 
   # TODO determine best move
   return [2, 3]
+
+
+def horizontal_check(player, board, direction):
+  if direction == 'top_to_bottom':
+    start, end, step = 0, len(board) - 2, 1
+  elif direction == 'bottom_to_top':
+    start, end, step = len(board) - 1, 1, -1
+
+  for j in range(len(board[0])):
+    for i in range(start, end, step):
+      if board[i][j] == player and board[i+step][j] != player and board[i+step][j] != 0:
+        while (i+2*step != end) and (board[i+2*step][j] != player):
+          if board[i+2*step][j] == 0:
+            valid_moves.append([i+2*step, j])
+          i += step
+      else:
+        continue
 
 
 def vertical_check(player, board, direction):
@@ -40,6 +59,23 @@ def vertical_check(player, board, direction):
         while (j+2*step != end) and (board[i][j+2*step] != player):
           if board[i][j+2*step] == 0:
             valid_moves.append([i, j+2*step])
+          j += step
+      else:
+        continue
+
+def diagonal_check(player, board, direction):
+  if direction == 'top_left_to_bottom_right':
+    start, end, step = 0, min(len(board), len(board[0])) - 2, 1
+  elif direction == 'bottom_right_to_top_left':
+    start, end, step = min(len(board), len(board[0])) - 1, 1, -1
+
+  for i in range(start, end, step):
+    for j in range(start, end, step):
+      if board[i][j] == player and board[i+step][j+step] != player and board[i+step][j+step] != 0:
+        while (i+2*step != end) and (j+2*step != end) and (board[i+2*step][j+2*step] != player):
+          if board[i+2*step][j+2*step] == 0:
+            valid_moves.append([i+2*step, j+2*step])
+          i += step
           j += step
       else:
         continue
